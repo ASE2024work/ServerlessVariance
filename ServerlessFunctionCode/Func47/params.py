be8300385a1101cbaeedfc29f9e24c4f842a499a
ASE2024work <2001111300@stu.pku.edu.cn>
2024-05-15 16:27:21 +0800

    update

--- ServerlessFunctionCode/Func47/params.py
+++
@@ -1,6 +0,0 @@
-accessKeyId="DummyID" 
-accessKey="DummyKey"
-bucketName="bucketwendycyn1"
-LambdaRoleARN="arn:aws:iam::XXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-StepRoleARN="arn:aws:iam::XXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-AccountID="1234567890"
